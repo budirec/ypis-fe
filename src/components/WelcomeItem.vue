83d4f98986d349083704602e208f952b5672250a
D-Tech <bd.dtech@thirdpxl.com>
2023-03-31 20:29:31 -0700

    Initial commit, Vue3 and Element Plus UI

---
+++ src/components/WelcomeItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-timeline-item :icon="icon" size="large" type="primary">
+    <el-card class="item">
+      <h1>
+        <slot name="heading"></slot>
+      </h1>
+      <p><slot></slot></p>
+    </el-card>
+  </el-timeline-item>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    icon: {
+      type: Object,
+      default: null
+    }
+  }
+}
+</script>
